1b9a21bcc73d256c305971ab675dff44fee3a20e
Ishara Dhanushan <isharadh2002@gmail.com>
2024-08-12 15:58:02 +0530

    Initializing the project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find Your Mechanic</title>
+</head>
+<body>
+    <h1>Welcome to our website</h1>
+</body>
+</html>

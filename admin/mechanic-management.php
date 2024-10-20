2abafcdeb5aa70673ad107c042c6667dc5db93ed
Ishara Dhanushan <isharadh2002@gmail.com>
2024-10-20 08:08:36 +0530

    Creating tables for mechanics and users

--- admin/mechanic-management.php
+++ admin/mechanic-management.php
@@ -15,7 +15,47 @@
 </head>
 <body>
     <?php
-        require "header.php"
+        require "header.php";
+        require "../shared/connect.php";
+    ?>
+    <?php
+    $query = "SELECT * FROM `mechanic`";
+    $result = mysqli_query($con,  $query);
+    $mechanics = [];
+    if (mysqli_num_rows($result) > 0):
+    ?>
+        <table class="user-table">
+            <tr class="table-heading-tr">
+                <th class="table-heading mechanicid">Mechanic ID</th>
+                <th class="table-heading userid">UserID</th>
+                <th class="table-heading username">Name</th>
+                <th class="table-heading email">Work Address</th>
+                <th class="table-heading phone">Work Phone Number</th>
+                <th class="table-heading address">Specification</th>
+                <th class="table-heading usertype">Is Approved</th>
+            </tr>
+            <?php
+            while ($row = mysqli_fetch_assoc($result)):
+                $mechanics[] = $row;
+                //print_r($row);
+            ?>
+                <tr class="table-data-tr">
+                    <td class="table-data"><?php echo $row['MechanicID'] ?></td>
+                    <td class="table-data"><?php echo $row['UserID'] ?></td>
+                    <td class="table-data"><?php echo $row['Name'] ?></td>
+                    <td class="table-data"><?php echo $row['WorkAddress'] ?></td>
+                    <td class="table-data"><?php echo $row['WorkPhoneNumber'] ?></td>
+                    <td class="table-data"><?php echo $row['Specification'] ?></td>
+                    <td class="table-data"><?php echo ($row['IsApproved']) ? "Approved" : "Not Approved"; ?></td>
+
+                </tr>
+            <?php
+            endwhile;
+            ?>
+        </table>
+    <?php
+    //print_r($mechanics);
+    endif;
     ?>
 </body>
 </html>
